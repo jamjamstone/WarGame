1565fc090fdc8f524ffbaa62f0c1049c1a5b7091
jamjamstone <junghyun4545@naver.com>
2025-04-24 13:24:07 +0900

    [Add] add room scene
    룸에서 사용할 씬 제작

--- Assets/Scripts/TitleUIManager.cs
+++ Assets/Scripts/TitleUIManager.cs
@@ -6,17 +6,30 @@ using UnityEngine;
 using UnityEngine.UI;
 using Photon.Pun;
 
-public class TitleUIManager : MonoBehaviour
+public class TitleUIManager : MonoBehaviourPunCallbacks
 {
     [SerializeField] Button createRoom;
     [SerializeField] Button joinRoom;
     [SerializeField] Button joinRandomRoom;
     [SerializeField] Button quitGame;
 
+    [SerializeField] TMP_InputField roomNameInput;
+    [SerializeField] TMP_Text roomNameLogo;
+    [SerializeField] Button joinOrCreateRoomButton;
+
+    private string roomName;
+    private bool isCreateRoom = false;
+    private bool isJoinRoom = false;
+    private void Awake()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
     // Start is called before the first frame update
     void Start()
     {
         
+        PhotonNetwork.JoinLobby();
+        roomNameInput.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
@@ -27,22 +40,51 @@ public class TitleUIManager : MonoBehaviour
 
     public void CreateRoomButtonPressed()
     {
-
+        roomNameInput.gameObject.SetActive(true);
+        isCreateRoom = true;
     }
     public void JoinRoomButtonPressed()
     {
-
+        roomNameInput.gameObject.SetActive(true);
+        isJoinRoom = true;
     }
     public void JoinRandomRoomButtonPressed()
     {
-
+        PhotonNetwork.JoinRandomOrCreateRoom();
     }
 
     public void QuitGameButtonPressed()
     {
-        
+        #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+        #else
+        Application.Quit(); // 어플리케이션 종료
+        #endif
     }
 
 
 
+    public void JOCRButtonClick()
+    {
+        roomName = roomNameInput.text;
+        if (isJoinRoom)
+        {
+            PhotonNetwork.JoinRoom(roomName);
+            isJoinRoom=false;
+
+        }
+
+        if (isCreateRoom)
+        {
+            PhotonNetwork.CreateRoom(roomName);
+            isCreateRoom=false;
+        }
+        roomNameInput.gameObject.SetActive(false);
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        base.OnConnectedToMaster();
+
+    }
 }
